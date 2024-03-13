862eebd6706d461eeaae61d9865f982836f32ed4
Artur Gęsiarz <arturgesiarz@student.agh.edu.pl>
2024-03-13 10:02:36 +0100

    Created full navbar and herosection with photo

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -7,8 +7,8 @@ const HeroSection = () => {
     return (
         <section className="grid grid-cols-1 lg:grid-cols-12 my-4">
             <div className="col-span-7 place-self-center place-items-center grid lg:place-items-start">
-                <h1 className="text-white max-w-2xl mb-4 lg:text-6xl text-4xl font-extrabold">
-                    <span className="text-transparent text-6xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+                <h1 className="text-white max-w-2xl mb-4 lg:text-5xl text-3xl font-extrabold">
+                    <span className="text-transparent lg:text-5xl text-3xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
                         Hello, I'm{" "}
                     </span>
                     <br/>
@@ -36,7 +36,7 @@ const HeroSection = () => {
                     <button className="bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white px-6 py-3 rounded-full mr-4">
                         Hire Me
                     </button>
-                    <button className="bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white px-6 py-3 rounded-full mr-4">
+                    <button className="m-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 px-1 py-1  text-white rounded-full">
                         <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                             Download CV
                         </span>
@@ -44,13 +44,13 @@ const HeroSection = () => {
                 </div>
             </div>
             <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-                <div className="bg-[#181818] lg:w-[400px] lg:h-[400px] w-[250px] h-[250px] rounded-full relative">
+                <div className="bg-[#181818] lg:w-[420px] lg:h-[420px] w-[250px] h-[250px] rounded-full relative">
                     <Image
-                        src=""
-                        alt=""
+                        src="/images/my_photo.png"
+                        alt="my image"
                         className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                        width={300}
-                        height={300}
+                        width={400}
+                        height={400}
                     />
                 </div>
             </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import Navbar from "@/app/components/Navbar";
+import HeroSection from "@/app/components/HeroSection";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-
+      <Navbar />
+      <div className="container mx-auto mt-24 px-12 py-4">
+        <HeroSection/>
+      </div>
     </main>
   );
 }

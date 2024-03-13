05cd2730ece1713f18df3e4721e751a5bec486ac
Artur Gęsiarz <arturgesiarz@student.agh.edu.pl>
2024-03-13 13:03:56 +0100

    Added my projects seciton

---
+++ src/app/components/ProjectSection.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import ProjectCard from "@/app/components/ProjectCard";
+
+const projectsData = [
+    {
+        id : 1,
+        title: "Example title 1",
+        description: "Project 1 description",
+        image: "/images/projects/1.jpg",
+        tag: ["All", "Web"],
+        gitUrl: "",
+        previewUrl: ""
+    },
+    {
+        id : 2,
+        title: "Example title 2",
+        description: "Project 2 description",
+        image: "/images/projects/2.jpg",
+        tag: ["All", "Web"],
+        gitUrl: "",
+        previewUrl: ""
+    },
+    {
+        id : 3,
+        title: "Example title 3",
+        description: "Project 3 description",
+        image: "/images/projects/3.jpg",
+        tag: ["All", "Web"],
+        gitUrl: "",
+        previewUrl: ""
+    },
+    {
+        id : 4,
+        title: "Example title 4",
+        description: "Project 4 description",
+        image: "/images/projects/4.jpg",
+        tag: ["All", "Web"],
+        gitUrl: "",
+        previewUrl: ""
+    },
+]
+
+const ProjectSection = () => {
+    return (
+        <>
+            <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+                My projects
+            </h2>
+            <div className="grid md:grid-cols-3 gap-8 md:gap-12">
+                {projectsData.map((project) => (
+                        <ProjectCard key={project.id}
+                                     title={project.title}
+                                     description={project.description}
+                                     imgUrl={project.image}
+                                     gitUrl={project.gitUrl}
+                                     previewUrl={project.previewUrl}
+                                     tags={project}/>
+                    ))
+                }
+            </div>
+        </>
+    )
+}
+
+export default ProjectSection
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import HeroSection from "@/app/components/HeroSection";
 import Navbar from "@/app/components/Navbar";
 import AboutSection from "@/app/components/AboutSection";
+import ProjectSection from "@/app/components/ProjectSection";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <div className="container mt-24  mx-auto px-12 py-4">
           <HeroSection />
           <AboutSection />
+          <ProjectSection />
       </div>
     </main>
   );

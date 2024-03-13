7b33d0f6b5e1b9da9db2e7ca764d4f2e97f85563
Artur Gęsiarz <arturgesiarz@student.agh.edu.pl>
2024-03-13 08:58:19 +0100

    Project init

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,61 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+
+const HeroSection = () => {
+    return (
+        <section className="grid grid-cols-1 lg:grid-cols-12 my-4">
+            <div className="col-span-7 place-self-center place-items-center grid lg:place-items-start">
+                <h1 className="text-white max-w-2xl mb-4 lg:text-6xl text-4xl font-extrabold">
+                    <span className="text-transparent text-6xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+                        Hello, I'm{" "}
+                    </span>
+                    <br/>
+                    <TypeAnimation
+                        sequence={[
+                            "Artur",
+                            1000,
+                            "Java Lover",
+                            1000,
+                            "C++ Enjoyer",
+                            1000,
+                            "Kotlin App Creator",
+                            1000,
+                        ]}
+                        wrapper="span"
+                        speed={50}
+                        repeat={Infinity}
+                    />
+                </h1>
+                <p className="text-[#ADB7BE] mb-6 textl-lg lg:text-xl">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+                    voluptatum.
+                </p>
+                <div>
+                    <button className="bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white px-6 py-3 rounded-full mr-4">
+                        Hire Me
+                    </button>
+                    <button className="bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white px-6 py-3 rounded-full mr-4">
+                        <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                            Download CV
+                        </span>
+                    </button>
+                </div>
+            </div>
+            <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+                <div className="bg-[#181818] lg:w-[400px] lg:h-[400px] w-[250px] h-[250px] rounded-full relative">
+                    <Image
+                        src=""
+                        alt=""
+                        className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                        width={300}
+                        height={300}
+                    />
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default HeroSection;

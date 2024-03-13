31fca41029c17da598e4e30a7370da7e833563cc
Artur Gęsiarz <arturgesiarz@student.agh.edu.pl>
2024-03-13 13:14:49 +0100

    Created ProjectTag

--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -46,6 +46,14 @@ const ProjectSection = () => {
             <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
                 My projects
             </h2>
+            <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+                <button className="rounded-full border-2 border-purple-500 px-6 py-3 text-xl cursor-pointer">
+                    All
+                </button>
+                <button className="rounded-full border-2 border-slate-600 hover:border-white px-6 py-3 text-xl cursor-pointer">
+                    Web
+                </button>
+            </div>
             <div className="grid md:grid-cols-3 gap-8 md:gap-12">
                 {projectsData.map((project) => (
                         <ProjectCard key={project.id}

0fdf3c6825d3312d24938a7872d99c1da12e859d
Artur Gęsiarz <arturgesiarz@student.agh.edu.pl>
2024-03-19 20:27:55 +0100

    Wrapping added

--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -75,7 +75,7 @@ const ProjectSection = () => {
             <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
                 My projects
             </h2>
-            <div className="text-white flex flex-row justify-center items-center gap-2 py-3">
+            <div className="text-white flex flex-wrap justify-center items-center gap-2 py-3">
                 <ProjectTag onClick={handleTagChange}
                             name="All"
                             isSelected={tag === "All"}
